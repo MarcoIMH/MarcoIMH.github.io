d1ae6780dd7022bab346e6f4c993a7d7a06c7cc2
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2019-12-23 03:55:04 +0100

    ADDED: Unit Factory
    Añadida las estadísticas de la factoría de unidades.

--- zgame/src/units/unitFactory.js
+++ zgame/src/units/unitFactory.js
@@ -3,4 +3,37 @@ export default class UnitFactory{
 		this.st = state;
 		this.ms = mapSel;
 	}
+
+	getUniConfig(type){
+		this.type = type;
+
+		//Reset config array
+		this.resetConfig = [];
+		this.unitSettings = this.resetConfig;
+		
+		/*-----------------------------------------------------------------------------------------------------------
+				Array Structure: HP / POISON ATTACK / ICE ATTACK / FIRE ATTACK / THUNDER ATTACK / TIME FAULT
+		------------------------------------------------------------------------------------------------------------*/
+		switch(this.ms){
+			case 1: {
+				if     (this.type == "light")	this.unitSettings = [125,7,0,0,0];
+				else if(this.type == "middle")	this.unitSettings = [220,0,0,14,0];
+				else if(this.type == "heavy")	this.unitSettings = [310,25,10,0,0];
+				break;
+			}
+			case 2:{
+				if     (this.type == "light")	this.unitSettings = [155,0,0,18,0];
+				else if(this.type == "middle")	this.unitSettings = [250,0,0,23,0];
+				else if(this.type == "heavy")	this.unitSettings = [600,0,0,35,0];
+				break;
+			} 			
+			case 3: {
+				if     (this.type == "light")	this.unitSettings = [220,0,16,0,0];
+				else if(this.type == "middle")	this.unitSettings = [410,0,0,0,25];
+				else if(this.type == "heavy")	this.unitSettings = [900,0,35,0,0];
+				break;
+			}
+		}			
+		return this.unitSettings;
+	}
 }
